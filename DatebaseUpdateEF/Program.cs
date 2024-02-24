0cd571038456fd3abf285a1135f4894b669815c2
huy <huy.dq1@bimgroup.com>
2024-02-24 11:57:29 +0700

    add 2 more ways to update batch entities

--- DatebaseUpdateEF/Program.cs
+++ DatebaseUpdateEF/Program.cs
@@ -29,6 +29,8 @@ app.UseHttpsRedirection();
 
 app.MapPut("update-salaries-by-company-basic", async (Guid companyId, ApplicationDBContext dbContext) =>
 {
+    // Why save change is slow : https://code-maze.com/efcore-bulk-updates/
+    
     var company = await dbContext.Set<Company>()
         .Include(o => o.Employees)
         .AsSplitQuery()
@@ -63,11 +65,14 @@ app.MapPut("update-salaries-by-company-sql-raw", async (Guid companyId, Applicat
         WHERE CompanyId = {companyId}";
 
     // Execute the SQL query and EF core in the same transaction to ensure consistency
-    await dbContext.Database.BeginTransactionAsync();
-    await dbContext.Database.ExecuteSqlInterpolatedAsync(query);
-    company.UpdateSalaryDate = DateTime.Now;
-    await dbContext.SaveChangesAsync();
-    await dbContext.Database.CommitTransactionAsync();
+    await using (var transaction = await dbContext.Database.BeginTransactionAsync())
+    {
+        await dbContext.Database.ExecuteSqlInterpolatedAsync(query);
+        company.UpdateSalaryDate = DateTime.Now;
+        await dbContext.SaveChangesAsync();
+        await transaction.CommitAsync();
+    }
+
 
     return Results.Ok($"Updated salaries for company {company.Name}");
 });
@@ -86,13 +91,64 @@ app.MapPut("update-salaries-by-company-dapper", async (Guid companyId, Applicati
         WHERE CompanyId = @CompanyId";
 
     // Execute dapper and EF core in the same transaction to ensure consistency
-    var currentTransaction = await dbContext.Database.BeginTransactionAsync();
-    await dbContext.Database.GetDbConnection().ExecuteAsync(query, new { CompanyId = companyId }, currentTransaction.GetDbTransaction());
-    company.UpdateSalaryDate = DateTime.Now;
-    await dbContext.SaveChangesAsync();
-    await currentTransaction.CommitAsync();
+
+    await using (var transaction = await dbContext.Database.BeginTransactionAsync())
+    {
+        await dbContext.Database.GetDbConnection()
+            .ExecuteAsync(query, new { CompanyId = companyId }, transaction.GetDbTransaction());
+        company.UpdateSalaryDate = DateTime.Now;
+        await dbContext.SaveChangesAsync();
+        await transaction.CommitAsync();
+    }
 
     return Results.Ok($"Updated salaries for company {company.Name}");
 });
+
+app.MapPut("update-salaries-by-company-ef-core-bulk-extension",
+    async (Guid companyId, ApplicationDBContext dbContext) =>
+    {
+        var company = await dbContext.Set<Company>().FirstOrDefaultAsync(o => o.Id == companyId);
+        if (company is null)
+        {
+            return Results.NotFound($"Not found company with id {companyId}");
+        }
+
+        var employees = await dbContext.Set<Employee>().Where(o => o.CompanyId == companyId).ToListAsync();
+        foreach (var employee in employees)
+        {
+            employee.Salary *= 1.1m;
+        }
+        
+        await using (var transaction = await dbContext.Database.BeginTransactionAsync())
+        {
+            await dbContext.BulkUpdateAsync(employees);
+            company.UpdateSalaryDate = DateTime.Now;
+            await dbContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+
+        return Results.Ok($"Updated salaries for company {company.Name}");
+    });
+
+app.MapPut("update-salaries-by-company-ef-core-bulk-after-version-7",
+    async (Guid companyId, ApplicationDBContext dbContext) =>
+    {
+        var company = await dbContext.Set<Company>().FirstOrDefaultAsync(o => o.Id == companyId);
+        if (company is null)
+        {
+            return Results.NotFound($"Not found company with id {companyId}");
+        }
+
+        await using (var transaction = await dbContext.Database.BeginTransactionAsync())
+        {
+            await dbContext.Set<Employee>().Where(o=>o.CompanyId == companyId)
+                .ExecuteUpdateAsync(s=>s.SetProperty(o=>o.Salary, o=>o.Salary * 1.1m));
+            company.UpdateSalaryDate = DateTime.Now;
+            await dbContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+
+        return Results.Ok($"Updated salaries for company {company.Name}");
+    });
 
 app.Run();
